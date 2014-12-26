5113f6e4d00bdcdc14843bee4074fb948ca6156d
jrmsx <jrms_x@hotmail.com>
2014-12-25 18:47:00 -0600

    Correcciones menores al modelo
    look and feel del sistema operativo huesped

--- src/com/puntoVenta/main.java
+++ src/com/puntoVenta/main.java
@@ -5,6 +5,11 @@
  */
 package com.puntoVenta;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 /**
  *
  * @author luis
@@ -12,7 +17,7 @@ package com.puntoVenta;
 public class main {
     
     public static void main(String[] args) {
-        
+        setLookAndFeel();
         Login ventanalogin = new Login();
         PanelLogin panel = new PanelLogin();
         OyenteLogin oyente = new OyenteLogin(panel,ventanalogin);
@@ -21,7 +26,16 @@ public class main {
         ventanalogin.addWindowListener(oyente);
         panel.addEventos(oyente);
         ventanalogin.add(panel);
+        
         ventanalogin.setVisible(true);
     }
+    
+    static void setLookAndFeel(){
+         try{
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }catch(Exception E){}
+    }
+
+   
     
 }
